c0a3623200dec23e32718674557e386b045e43ef
proffArtista <kibzjeff@gmail.com>
2020-08-17 20:04:17 +0300

    workbox comfypen

---
+++ public/precache-manifest.fbeec1b8daa832036c03472717a6b956.js
@@ -0,0 +1,91 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a130ef5fdeb60cf38d57d6abb245cc61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200717575e6c0e861736",
+    "url": "/static/css/2.717d6787.chunk.css"
+  },
+  {
+    "revision": "f0d9d09dd3ad1397c314",
+    "url": "/static/css/main.824755ff.chunk.css"
+  },
+  {
+    "revision": "200717575e6c0e861736",
+    "url": "/static/js/2.2b15d64f.chunk.js"
+  },
+  {
+    "revision": "eb819d9510c75a484cddedc7b93aaf92",
+    "url": "/static/js/2.2b15d64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d9d09dd3ad1397c314",
+    "url": "/static/js/main.373dc3c6.chunk.js"
+  },
+  {
+    "revision": "1ca72088139894b75325",
+    "url": "/static/js/runtime-main.013894e1.js"
+  },
+  {
+    "revision": "b9be3d1a238b09107ba4676a697c9f8d",
+    "url": "/static/media/diary144.b9be3d1a.png"
+  },
+  { "revision": "4", "url": "/css/bootstrap.min.css" },
+  { "revision": null, "url": "/css/prism.css" },
+  { "revision": "4", "url": "/js/bootstrap.bundle.min.js" },
+  { "revision": "4", "url": "/js/jquery.min.js" },
+  { "revision": "4", "url": "/js/popper.min.js" },
+  { "revision": "4", "url": "/js/prism.js" },
+  { "revision": null, "url": "/tinymce/tinymce.min.js" },
+  { "revision": null, "url": "/tinymce/jquery.tinymce.min.js" },
+  { "revision": null, "url": "/tinymce/themes/mobile/theme.min.js" },
+  { "revision": null, "url": "/tinymce/themes/silver/theme.min.js" },
+  { "revision": null, "url": "/tinymce/skins/ui/oxide/content.min.css" },
+  { "revision": null, "url": "/tinymce/skins/ui/oxide/content.mobile.min.css" },
+  { "revision": null, "url": "/tinymce/skins/ui/oxide/skin.min.css" },
+  { "revision": null, "url": "/tinymce/skins/ui/oxide/skin.mobile.min.css" },
+  { "revision": null, "url": "/tinymce/skins/content/default/content.min.css" },
+  {
+    "revision": null,
+    "url": "/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": null,
+    "url": "/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff?8x92w3"
+  },
+  { "revision": null, "url": "/tinymce/plugins/advlist/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/anchor/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/autolink/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/autosave/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/charmap/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/colorpicker/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/emoticons/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/emoticons/js/emojis.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/fullpage/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/fullscreen/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/help/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/hr/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/insertdatetime/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/link/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/lists/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/media/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/nonbreaking/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/pagebreak/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/paste/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/preview/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/print/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/quickbars/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/searchreplace/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/spellchecker/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/table/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/textcolor/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/textpattern/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/visualblocks/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/visualchars/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/wordcount/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/code/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/template/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/image/plugin.min.js" },
+  { "revision": null, "url": "/tinymce/plugins/media/plugin.min.js" }
+]);
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{json,css,js,flow,png,xml,ico,html,md,txt,woff}"
+  ],
+  "swDest": "public/sw.js"
+};
